192e0737b1b2d0c11223b60389057e1d1c008fa2
Rafael Magno <rafaelmagno125@gmail.com>
2025-02-08 20:01:44 -0300

    fix: ajuste/correcao if else

--- js/app.js
+++ js/app.js
@@ -16,12 +16,8 @@ function getCategorySelect(){
     const select = document.querySelector("select")
     if(select.value === 'petiscos'){
        window.location.href = 'https://rafaelmagnoc.github.io/kabana-caju-cardapio/'
-    }
-    
-    else if(select.value === 'bebidas'){
-       window.location.href = 'https://github.com/RafaelMagnoC/kabana-caju-cardapio-bebidas'
-    }
+    }    
     else{
-      window.location.href = 'https://rafaelmagnoc.github.io/kabana-caju-cardapio/'
+      window.location.href = 'https://github.com/RafaelMagnoC/kabana-caju-cardapio-bebidas'
     }
 }
